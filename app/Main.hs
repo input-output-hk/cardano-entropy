2067a9450355a3a68c0e92f1502d91a8fc52b478
John Ky <john.ky@iohk.io>
2021-03-22 08:25:55 +1100

    Initial main

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Data.Function
+
+import qualified Test.WebDriver as WD
+
+chromeConfig :: WD.WDConfig
+chromeConfig = WD.defaultConfig
+  & WD.useBrowser WD.chrome
+
 main :: IO ()
-main = return ()
+main = WD.runSession chromeConfig $ do
+  WD.openPage "http://google.com"
+
+  searchInput <- WD.findElem ( WD.ByCSS "input[type='text']" )
+
+  WD.sendKeys "Hello, World!" searchInput
+  WD.submit searchInput
+  WD.closeSession
